26d9ff3bbfe0c4d97605bbb2036172b761f8936d
JordiLara <jordilarareyes@gmail.com>
2024-12-11 16:55:03 +0100

    actualize calendar ts types

--- src/types/calendar.ts
+++ src/types/calendar.ts
@@ -1,20 +1,6 @@
-export interface CalendarEvent {
-  id?: number;
-  routeId: number;
-  title: string;
-  startDate: string;
-  startTime: string;
-  description?: string;
-  meetingPoint?: string;
-  createdAt?: string;
-  updatedAt?: string;
-}
-
-export interface EventFormData {
-  routeId: number;
-  title: string;
-  startDate: string;
-  startTime: string;
-  description: string;
-  meetingPoint: string;
+export interface Event {
+  id: number;
+  fecha: string; 
+  hora_salida: string; 
+  ruta_id: number; 
 }
